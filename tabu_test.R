2a2a5dc4a9d552a1046fbd104c18e016e2fd7a08
Denise <denise.meerkerk@gmail.com>
2020-01-27 14:29:54 +0100

    tabu dataframe number of parents and number of children made
    in tabuNW_parentchild this dataframe can be found

--- tabu_test.R
+++ tabu_test.R
@@ -19,8 +19,8 @@ d <- subset(cbsData, select = c(
 ###
 testCBS <- cbsData[,-(1:4)]
 testCBS[] <- lapply(testCBS, as.factor)
-thing <- tabu(d, debug = TRUE, score = "loglik-g",maxp = 4, tabu = 12)
-BIC(thing, d)
+tabuNW <- tabu(d, debug = TRUE, score = "loglik-g",maxp = 4, tabu = 12)
+BIC(tabuNW, d)
 #possible parameters to adjust for: 
 #   score (https://www.bnlearn.com/documentation/man/network.scores.html),
 #   maxp (maximum number of parents of a node.)
@@ -29,21 +29,25 @@ BIC(thing, d)
 #   max.iter 	(an integer, the maximum number of iterations.)
 
 plot.new()
-graphviz.plot(thing, shape = "ellipse", highlight = list(nodes = c("CRIME_TOTAL","FEMALE_PCT") , col = "blue",textCol = "blue"))
+graphviz.plot(tabuNW, shape = "ellipse", highlight = list(nodes = c("CRIME_TOTAL","FEMALE_PCT") , col = "blue",textCol = "blue"))
         
 tabu_dag_temp <- 0      
-for(i in 1:nrow(thing$arcs)){
-  tabu_dag_temp[i] <- paste(toString(thing$arcs[i,1]) , '->' , toString(thing$arcs[i,2]), '\n')
+for(i in 1:nrow(tabuNW$arcs)){
+  tabu_dag_temp[i] <- paste(toString(tabuNW$arcs[i,1]) , '->' , toString(tabuNW$arcs[i,2]), '\n')
 }
 
 tabu_nodes <- 0      
-for(i in 1:nrow(thing$nodes)){
-  tabu_nodes[i] <- paste(toString(thing$nodes[i]))
+for(i in 1:length(tabuNW$nodes)){
+  tabu_nodes[i] <- names(tabuNW$nodes[i])
   }
 tabu_dag_temp <- toString(tabu_dag_temp)
 tabu_dag_temp
 tabu_dag_temp <- gsub(',','', tabu_dag_temp)
 dagtemp <- dagitty(paste('dag{', tabu_nodes, tabu_dag_temp, '}'))
 
-
-
+tabuNW_parentchild <- matrix(ncol = 3, nrow = length(tabuNW$nodes))
+for(i in 1:length(tabuNW$nodes)){
+  tabuNW_parentchild[i,] <- c(tabu_nodes[i],length(tabuNW$nodes[[i]]$parents), length(tabuNW_dss_aa <- tabuNW$nodes[[i]]$children))
+  tabuNW_parentchild
+}
+colnames(tabuNW_parentchild) <- c('Variable Name','Parent','Child')
