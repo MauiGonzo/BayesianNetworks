a9d7ad0fd2dc225150529ed9284b1245a41f871f
Denise <denise.meerkerk@gmail.com>
2020-01-27 23:52:42 +0100

    Update on the parameter adjustment of tabu
    also pictures of the networks added

--- tabuParameterAdjusting.R
+++ tabuParameterAdjusting.R
@@ -2,11 +2,16 @@
 score_methods <- c("loglik-g", "aic-g", "bic-g", "bge") #"pred-loglik-g", : Error in per.node.score(network = start, score = score, targets = nodes,  : REAL() can only be applied to a 'numeric', not a 'NULL'
 nr_tests= length(score_methods)
 tabu_avg_parentchild = matrix(ncol = 2, nrow = nr_tests)
-for(tabu_list_size in 1:nr_tests){
-  tabuNW_temp <- tabu(d, score =score_methods[tabu_list_size], maxp = Inf)
+for(score in 1:nr_tests){
+  tabuNW_temp <- tabu(d, score =score_methods[score], maxp = Inf)
   #plot.new()
+  png(filename = paste("Project2Figures/", score_methods[score], ".png", sep=''),
+      width = 1200,
+      height = 1200,
+      bg = "transparent",
+      res = 500)
   graphviz.plot(tabuNW_temp, shape = "ellipse", highlight = list(nodes = c("CRIME_TOTAL","FEMALE_PCT") , col = "blue",textCol = "blue"))
-  
+  dev.off()
   tabu_nodes <- 0      
   for(i in 1:length(tabuNW_temp$nodes)){
     tabu_nodes[i] <- names(tabuNW_temp$nodes[i])
@@ -20,7 +25,7 @@ for(tabu_list_size in 1:nr_tests){
     }
   
   colnames(tabuNW_temp_parentchild) <- c('Variable Name','Parent','Child')
-  tabu_avg_parentchild[tabu_list_size,] <- c(mean(as.numeric(tabuNW_temp_parentchild[,2])),
+  tabu_avg_parentchild[score,] <- c(mean(as.numeric(tabuNW_temp_parentchild[,2])),
                                              mean(as.numeric(tabuNW_temp_parentchild[,3])))
   print(mean(as.numeric(tabuNW_temp_parentchild[,2])))
         }
